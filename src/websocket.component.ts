2eb81e10b6a0429817f93ffcf34ac19b0c2fd86f
Emeric LEBON <emeric.lebon@gmail.com>
2017-08-09 22:33:02 +0200

    Services are initialized once (after all, they must be Singleton). At every new socket, services are notified. If there's a valid user found in headers, it is sent along with its associated socket.

--- src/websocket.component.ts
+++ src/websocket.component.ts
@@ -1,30 +1,27 @@
-import { IContainer } from 'vulcain-corejs';
-import { IWs } from './wsAdapter';
+import {IContainer} from 'vulcain-corejs';
+import {IWs} from './wsAdapter';
 
 export class WebSocketComponent {
 
 
     /**
-     * 
-     * @param container 
-     * @param io 
+     *
+     * @param container
+     * @param io
      * @param services This is a list of websocket service who will be listened
      */
-    constructor(private container: IContainer, private io: SocketIO.Server, private services: Array<string>) {
+    constructor(private container: IContainer, private io: SocketIO.Server, private services: Array<IWs>) {
         this.initialize();
     }
+
     /**
-     * 
-     * @param socket 
+     *
+     * @param socket
      */
-    newSocketHappen(socket: SocketIO.Socket) {
+    newSocketHappen(socket: SocketIO.Socket, tokenResolved: any) {
         // Loop on all service to attach the new socket
-        this.services.forEach((serviceName: string) => {
-            // Use the container to load the instance of service
-            let service = this.container.get<IWs>(serviceName);
-            service.init(this.io, socket);
-            // Listen eventName on socket
-            // https://socket.io/docs/server-api/#socket-on-eventname-callback
+        this.services.forEach((service: IWs) => {
+            service.newSocket(socket, tokenResolved);
             socket.on(service.onSetEventName(), (msg) => {
                 service.onCall(msg);
             });
--- src/websocket.service.ts
+++ src/websocket.service.ts
@@ -1,20 +1,21 @@
-import {AbstractAdapter, IContainer, Inject} from 'vulcain-corejs';
-import { Injectable, LifeTime } from 'vulcain-corejs/dist';
+import {AbstractAdapter, IContainer, IDynamicProperty, Inject, System} from 'vulcain-corejs';
+import {Injectable, LifeTime} from 'vulcain-corejs/dist';
 //
-import { WebSocketComponent } from "./websocket.component";
+import {WebSocketComponent} from "./websocket.component";
 import {TokenService} from "vulcain-corejs/dist/defaults/services/tokenService";
+import {IWs} from "./wsAdapter";
+import Socket = SocketIO.Socket;
 
 const SocketIo = require('socket.io');
 
 @Injectable(LifeTime.Singleton, 'WebSocketService')
 export class WebSocketService {
-    private services: string[];
+    private services: IWs[] = [];
     private container: IContainer;
 
     private io: SocketIO.Server;
-
-    constructor(@Inject('TokenService') private tokenService: TokenService) {
-    }
+    private tokenService: TokenService;
+    private tokenKey: IDynamicProperty<string>;
 
     /**
      *
@@ -22,12 +23,11 @@ export class WebSocketService {
      * @param server The instance of express Server to attach socket.io on it
      * @param services This is a list of websocket service who will be listened
      */
-    start(container: IContainer, server: AbstractAdapter, services: Array<string>) {
+    start(container: IContainer, server: AbstractAdapter, services: Array<string>, tokenService?: TokenService) {
         this.container = container;
         this.io = new SocketIo(server);
-        this.services = services;
-        console.log(this.tokenService);
 
+        this.tokenService = tokenService;
         // this.container.injectFrom(pathWs);
 
         this.initializeListener();
@@ -35,9 +35,39 @@ export class WebSocketService {
 
     private initializeListener() {
         let ws = new WebSocketComponent(this.container, this.io, this.services);
-        this.io.on('connection', (socket) => {
+        this.io.on('connection', async (socket) => {
             // console.log('User connected');
-            ws.newSocketHappen(socket);
+            let tokenResolved: any = await this.getUserToken(socket);
+            ws.newSocketHappen(socket, tokenResolved);
         });
+    }
+
+    private initializeServices(services: string[]) {
+        services.forEach((serviceName: string) => {
+            let service = this.container.get<IWs>(serviceName);
+            service.init(this.io);
+            this.services.push(service);
+        });
+    }
+
+    private async getUserToken(socket: Socket): Promise<any> {
+        // get tokenservice or return null
+        if (!this.tokenService) {
+            return null;
+        }
+        try {
+            // get authorization header or return null
+            const authorizationHeader = socket.handshake.headers['Authorization'];
+            if (!authorizationHeader && (!authorizationHeader.startsWith("Bearer "))) {
+                return null;
+            }
+            let token: string = authorizationHeader.slice(7); // Removing "Bearer "
+            // resolve token or return null
+            return await this.tokenService.verifyTokenAsync({token: token, tenant: ""});
+        }
+        catch (error) {
+            return null;
+        }
+
     }
 }
